b21522277755647e87645fb59ad563253888508b
Sanket Desai <sanket.desai@ecanarys.com>
2024-03-21 12:47:35 +0530

    another commit

--- dist/index.js
+++ dist/index.js
@@ -13932,13 +13932,15 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getSummaryMarkdown = exports.writeSummary = exports.addToSummary = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 function addToSummary(title, alerts) {
-    const headers = ['Alert Number', 'Secret State', 'Secret Type', 'HTML URL', 'Repo Name', 'Repo Owner'];
+    const headers = ['Alert Number', 'Secret State', 'Secret Type', 'HTML URL', 'Repo Name', 'Repo Owner', 'Org Name', 'Org Owner'];
     // Define the table rows
     const rows = alerts.map(alert => [
         alert.number.toString(),
         alert.state,
         alert.secret_type,
         alert.html_url,
+        alert.name,
+        alert.login,
         alert.html_url,
         alert.html_url
     ]);
